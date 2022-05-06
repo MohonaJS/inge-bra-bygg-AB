04794338f42a15842cf695946ed4f3a81f802998
Tamisra <tamisra@MacBook-Pro-som-tillhor-Tamisra.local>
2022-05-06 18:49:46 +0200

    client controller add but need to fix functions

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -1,13 +1,22 @@
+const db_config = require("../configs/db_config");
+const Task_message = db_config.task_message;
+
 const dashboard = (req, res) => {
   res.send("client Dashboard");
 };
 
-const createMessage = (req, res) => {
+const createMessage = async (req, res) => {
   let { message } = req.body;
-  res.send(message);
+  await Task_message.create(req.body);
+};
+
+const seeMyTask = async (req, res) => {
+  let { message } = req.body;
+  res.send("here is my task");
 };
 
 module.exports = {
   dashboard,
   createMessage,
+  seeMyTask,
 };
